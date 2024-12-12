d2a9e1e9f95da2edd8b9c726579f466d6cef07d5
BoxBoxJason <loickgoupil@yahoo.fr>
2024-12-12 11:09:38 +0100

    Add random snake race

--- snake/src/main/java/org/snakeinc/snake/model/Snake.java
+++ snake/src/main/java/org/snakeinc/snake/model/Snake.java
@@ -3,14 +3,38 @@ package org.snakeinc.snake.model;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Snake {
 
     private final ArrayList<Tile> body;
+    private Race race;
+
+    private enum Race {
+        PYTHON(Color.GREEN),
+        ANACONDA(Color.GRAY),
+        BOA(Color.BLUE);
+
+        private final Color color;
+
+        Race(Color color) {
+            this.color = color;
+        }
+
+        public Color getColor() {
+            return color;
+        }
+
+        public static Race getRandomRace() {
+            Race[] races = values();
+            return races[new Random().nextInt(races.length)];
+        }
+    }
 
     public Snake() {
         body = new ArrayList<>();
         body.add(new Tile(5, 5)); // La tête du serpent
+        this.race = Race.getRandomRace();
     }
 
     public ArrayList<Tile> getBody() {
@@ -21,8 +45,8 @@ public class Snake {
         return body.getFirst();
     }
 
-    public void eat(Apple apple) {
-        body.add(apple.getPosition());
+    public void eat(Food food) {
+        body.add(food.getPosition());
     }
 
     public void move(char direction) {
@@ -36,7 +60,7 @@ public class Snake {
                 newHead.setY(newHead.getY() + 1);
                 break;
             case 'L':
-                newHead.setX(newHead.getX() + 1);
+                newHead.setX(newHead.getX() - 1);
                 break;
             case 'R':
                 newHead.setX(newHead.getX() + 1);
@@ -49,7 +73,7 @@ public class Snake {
 
     public void draw(Graphics g) {
         for (Tile t : body) {
-            g.setColor(Color.GREEN);
+            g.setColor(race.getColor());
             t.drawRectangle(g);
         }
     }
