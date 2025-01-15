a5b792b7254f5597f577f0fbd8a7a259cb27b5da
BoxBoxJason <loickgoupil@yahoo.fr>
2025-01-15 11:29:09 +0100

    Fix tests for regular snake

--- snake/src/test/java/org/snakeInc/snake/SnakeTest.java
+++ snake/src/test/java/org/snakeInc/snake/SnakeTest.java
@@ -4,23 +4,45 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.snakeinc.snake.model.Food;
 import org.snakeinc.snake.model.Snake;
+import org.snakeinc.snake.model.Tile;
 
 public class SnakeTest {
 
     @Test
-    public void snakeEatApples_ReturnsCorrectBodySize() {
+    public void snakeEatApples_NoException() {
         Snake snake = new Snake();
         Food food = new Food();
-        snake.eat(food);
-        Assertions.assertEquals(2, snake.getBody().size());
-
+        Assertions.assertDoesNotThrow(() -> snake.eat(food));
     }
 
-    @Test void snakeMovesUp_ReturnCorrectHead() {
+    void testMoveDirection(Snake.Direction direction, int x, int y) {
         Snake snake = new Snake();
-        snake.move(Snake.Direction.UP);
-        Assertions.assertEquals(5, snake.getHead().getX());
-        Assertions.assertEquals(5, snake.getHead().getY());
+        Tile head = snake.getHead();
+        int startX = head.getX();
+        int startY = head.getY();
+        snake.move(direction);
+        Assertions.assertEquals(startX + x, snake.getHead().getX());
+        Assertions.assertEquals(startY + y, snake.getHead().getY());
+    }
+
+    @Test
+    void snakeMovesDown() {
+        testMoveDirection(Snake.Direction.DOWN, 0, 1);
+    }
+
+    @Test
+    void snakeMovesLeft() {
+        testMoveDirection(Snake.Direction.LEFT, -1, 0);
+    }
+
+    @Test
+    void snakeMovesUp() {
+        testMoveDirection(Snake.Direction.UP, 0, -1);
+    }
+
+    @Test
+    void snakeMovesRight() {
+        testMoveDirection(Snake.Direction.RIGHT, 1, 0);
     }
 
 }
